0e9788617d7a9073cd8a7eac09d0df8805c4a331
vetarulla <vetarulla@gmail.com>
2022-11-20 18:39:45 -0300

    Remove git conflicts notations

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ const APP_PORT = process.env.PORT || 3333;
 const path = require('path');
 const publicFolderPath = path.resolve('public');
 const methodOverride = require('method-override');
-//<<<<<<< HEAD
-
 
 //routes required
-const mainRouter = require ('./src/routers/mainRouter');
-const userLoginRouter = require ('./src/routers/userLoginRouter');
-const userRegisterRouter = require ('./src/routers/userRegisterRouter');
+const mainRouter = require('./src/routers/mainRouter');
+const userLoginRouter = require('./src/routers/userLoginRouter');
+const userRegisterRouter = require('./src/routers/userRegisterRouter');
 const cartProducts = require('./src/routers/cartRouter')
 const productDetail = require('./src/routers/productDetailRouter')
 const addProduct = require('./src/routers/addProductRouter')
@@ -23,11 +21,10 @@ app.use(express.static(publicFolderPath));
 
 // ejs 
 //=======
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 
-//>>>>>>> aff53be8bea2c47599c24673164f3311363efcbd
 app.set('view engine', 'ejs')
 app.set('views', './src/views')
 //
