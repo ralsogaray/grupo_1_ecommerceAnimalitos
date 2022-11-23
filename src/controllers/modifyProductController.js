3ef935d90a27c0a1e49b4bc2842fd3ef98c5ec9e
Onilot <onilotlr@gmail.com>
2022-11-23 19:21:27 -0300

    Se agrega ruta y metodo DELETE

--- src/controllers/modifyProductController.js
+++ src/controllers/modifyProductController.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const { nextTick } = require("process");
 
 const productsFilePath = path.join(__dirname, "../data/products.json");
 const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
@@ -7,21 +8,40 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 //const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const renderModifyProduct = {
-    modify: (req, res) => {
-        return res.render('users/modifyProduct.ejs')
-    },
     edit: (req, res) => {
         const productId = req.params.productId;
-    const productToFind = products.find(
-        (product) => product.id == productId
-    );
-    if(productToFind == undefined){
-        return res.send('No existe ese producto')
+        const productToFind = products.find(
+            (product) => product.id == productId
+        );
+        if (productToFind == undefined) {
+            return res.send('No existe ese producto')
+        }
+        return res.render('users/modifyProduct', {
+            product: productToFind
+        })
+    },
+    modify: (req, res) => {
+        const productId = req.params.productId;
+        const productToFind = products.find(
+            (product) => product.id == productId
+        );
+        if (productToFind == undefined) {
+            return res.send('No existe ese producto')
+        }
+        return res.render('users/deleteProduct', {
+            product: productToFind
+        })
+    },
+    delete: (req, res) => {
+        const productId = req.params.productId;
+        const productToDelete = products.find(
+            (product) => product.id == productId
+        );
+        if (productToDelete) {
+            products.filter(product => product.id !== id);
+        } else
+            res.render('./products/index.ejs');
     }
-    return res.render('users/modifyProduct', {
-        product: productToFind
-    })
-    }    
 }
 
-module.exports = {renderModifyProduct}
+module.exports = { renderModifyProduct }
--- src/routers/modifyProductRouter.js
+++ src/routers/modifyProductRouter.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const modifyProductController = require('../controllers/modifyProductController')
 
-router.get("/modifyProduct", modifyProductController.renderModifyProduct.modify)
+//router.get("/modifyProduct", modifyProductController.renderModifyProduct.modify)
 
 //** EDIT ONE PRODUCT */
 
 router.get("/modifyProduct/:productId/", modifyProductController.renderModifyProduct.edit)
-
+router.get("/deleteProduct/:productId/", modifyProductController.renderModifyProduct.modify)
+router.delete("/deleteProduct/:productId/", modifyProductController.renderModifyProduct.delete)
 
 module.exports = router
