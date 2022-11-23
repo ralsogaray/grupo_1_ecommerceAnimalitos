1e4272cf6634681d0caf168791aa932ca5b33760
ralsogaray <rodrigo.alsogaray91@gmail.com>
2022-11-23 15:36:59 -0300

    Se actualiza mainController para poder renderizar index utilizando informacion de archivo JSON

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,25 @@
+const fs = require("fs");
+const path = require("path");
 
+const productsFilePath = path.join(__dirname, "../data/products.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, {encoding: 'utf-8'}))
 
 const renderHome = (req,res) => {
-    return res.render(('index.ejs'))
+
+    
+    const catFood = products.filter(product => product.category == 'catFood')
+    const dogFood = products.filter(product => product.category == 'dogFood')
+    
+    const viewProducts = {
+        catFood,
+        dogFood
+    }
+
+    //return  res.send(viewProducts)
+    return res.render('index.ejs', viewProducts)
 }
 
 module.exports = {
     renderHome
 }
+
